0922040c4cac0813ea3bc38b546a1381536c6e24
Yibo Guo <nil.yibo@gmail.com>
2022-10-13 21:02:32 -0700

    Added setup script for simulator.

---
+++ sim/scripts/setup.sh
@@ -0,0 +1,36 @@
+#!/bin/zsh
+
+cd "$(dirname "$0")" || exit 1
+
+set -e
+
+echo "Installing build dependencies ..."
+
+sudo apt-get update
+sudo apt-get install -y zsh git build-essential
+sudo apt-get install -y cmake gcc-9 g++-9 libomp-dev libboost-all-dev
+
+echo "Done"
+echo
+
+
+echo "Pulling abseil-cpp submodule ..."
+
+pushd ../abseil-cpp
+git submodule init
+git submodule update
+popd
+
+echo "Done"
+echo
+
+
+echo "Building simulator ..."
+
+./build.sh -f
+
+echo "Done"
+echo
+
+[ -x "../build/release/pnet/pnet_sim" ] || { echo 'Simulator not found in build output!'; exit 1 }
+echo "Setup done. Simulator is now ready to use."
